90d17a6d9ecf683e763f6691f46911319afde305
Tim <github@timgreen.ws>
2020-05-25 17:34:20 +1000

    added powered by IPinfo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
             return (
                 <div className="row">
                     <div className="column">
-                           <h5> <a href="https://ipinfo.io/" target="_blank" rel="noopener noreferrer" class="fas fa-map-marker-alt" style={{ fontSize: '2em', marginTop: '0.1em', display: 'none' }}></a>
+                           <h5> <a href="https://ipinfo.io/" target="_blank" rel="noopener noreferrer" class="fas fa-map-marker-alt" style={{ fontSize: '2em', marginTop: '0.1em', display: 'none' }}>Powered by IPinfo</a>
                            </h5>
                     </div>
                 </div>
